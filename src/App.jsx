ee2cdbe1fb37e77b53e9196d4bb8311fb2f0ff83
silvajpedro <belojpedro.31@gmail.com>
2023-03-18 19:48:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Components/header.jsx"
+import Main from "./Components/main.jsx"
+import Footer from "./Components/footer.jsx"
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
---
+++ src/Components/header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>INICIO</li>
+            <li>NOTICIAS</li>
+            <li>CONTATOS</li>
+          </ul>
+        </nav>
+      </header>
+      <div className="backgroundDisney"></div>
+    </>
+  );
+}
